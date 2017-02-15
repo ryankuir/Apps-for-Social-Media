f6e9a2d0e7cc057fec79fcc107649f26426934e3
ryankuir <q5040181@live.tees.ac.uk>
2017-02-15 12:36:08 +0000

    First Commit
    Intial Movment and GitHub test upload.

---
+++ CarGame/Assets/Scripts/CarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+    float speedForce = 10f;
+    float torqueForce = 2f;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        GetComponent<Rigidbody>().AddForce(transform.forward * speedForce); // adds force sending the vehicle forward
+        
+        float turn = Input.GetAxis("Horizontal"); // find the horizontal key binds
+        rb.AddTorque(transform.up * torqueForce * turn);
+
+    }
+
+}
